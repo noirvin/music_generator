784799a1c172888be7f599e068556c3973886ab6
noirvin <vin.sp97@googlemail.com>
2020-03-23 02:05:42 -0700

    building parser to read midi and feed markov

--- midi_parser.py
+++ midi_parser.py
@@ -19,4 +19,36 @@ class MidiParser:
 
         self.markov_chain = MarkovChain
 
-    
+    def parse(self):
+
+        """
+        reads midi and makes markov insertions
+
+        """
+
+        midi = mido.MidiFile(self.filename)
+        self.ticks = midi.ticks_per_beat
+
+        prev = []
+        curr = []
+
+        for track in midi.tracks:
+
+            for msg in track:
+                if False:
+                    print(msg)
+
+                if msg.type is "set_tempo":
+                    self.tempo = msg.tempo
+
+                elif msg.type is "note_on":
+                    if msg.time is 0:
+                        curr.append(msg.note)
+                    else:
+                        self.make_transition(prev,curr,msg.time)
+
+    def make_transition(prev,curr,time):
+        """
+        inserts alk state transition possibilities into markov
+
+        """
